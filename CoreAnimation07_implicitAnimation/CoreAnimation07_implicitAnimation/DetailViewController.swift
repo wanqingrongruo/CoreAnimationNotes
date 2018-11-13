f92f4b3760c6651bb6c2dc0afc1e2cca81975eda
devzheng <1210538084@qq.com>
2018-11-13 15:52:26 +0800

    推进过度

---
+++ CoreAnimation07_implicitAnimation/CoreAnimation07_implicitAnimation/DetailViewController.swift
@@ -0,0 +1,54 @@
+//
+//  DetailViewController.swift
+//  CoreAnimation07_implicitAnimation
+//
+//  Created by roni on 2018/11/13.
+//  Copyright © 2018 roni. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    var tag = 0
+
+    @IBOutlet weak var containerView: UIView!
+    @IBOutlet weak var redView: UIView!
+
+    var colorLayer: CALayer = CALayer()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tag = 1
+        /*
+        // UIView 禁用隐式动画的方法
+       // 每个UIView对它关联的图层都扮演了一个委托，并且提供了-actionForLayer:forKey的实现方法。当不在一个动画块的实现中，UIView对所有图层行为返回nil，但是在动画block范围之内，它就返回了一个非空值
+
+        let outsideDelegate = redView.action(for: redView.layer, forKey: "backgroundColor")
+        print("outSide: \(String(describing: outsideDelegate))") // null
+        UIView.beginAnimations(nil, context: nil)
+        let insideDelegate = redView.action(for: redView.layer, forKey: "backgroundColor")
+        print("inSide: \(String(describing: insideDelegate))")
+        UIView.commitAnimations()
+    */
+
+        colorLayer.frame = CGRect(x: 0, y: 0, width: redView.bounds.width, height: redView.bounds.height)
+        colorLayer.backgroundColor = UIColor.blue.cgColor
+        redView.layer.addSublayer(colorLayer)
+
+        let transition = CATransition()
+        transition.type = .push
+        transition.subtype = CATransitionSubtype.fromLeft
+        colorLayer.actions = ["backgroundColor": transition]
+    }
+
+    @IBAction func changeColor(_ sender: UIButton) {
+        let red = CGFloat.random(in: 0 ... 1)
+        let green = CGFloat.random(in: 0 ... 1)
+        let blue = CGFloat.random(in: 0 ... 1)
+        colorLayer.backgroundColor = UIColor(displayP3Red: red, green: green, blue: blue, alpha: 1).cgColor
+    }
+
+    var callback: ((_ tag: Int) -> Void)?
+
+}
--- CoreAnimation07_implicitAnimation/CoreAnimation07_implicitAnimation/ViewController.swift
+++ CoreAnimation07_implicitAnimation/CoreAnimation07_implicitAnimation/ViewController.swift
@@ -15,27 +15,45 @@ class ViewController: UIViewController {
     var colorLayer: CALayer = CALayer()
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        colorLayer.frame = CGRect(x: 0, y: 0, width: redView.bounds.width, height: redView.bounds.height)
-        colorLayer.backgroundColor = UIColor.blue.cgColor
-        redView.layer.addSublayer(colorLayer)
+//        colorLayer.frame = CGRect(x: 0, y: 0, width: redView.bounds.width, height: redView.bounds.height)
+//        colorLayer.backgroundColor = UIColor.blue.cgColor
+//        redView.layer.addSublayer(colorLayer)
+
+        redView.layer.backgroundColor = UIColor.blue.cgColor
     }
 
     @IBAction func changeColor(_ sender: UIButton) {
         CATransaction.begin()
         CATransaction.setAnimationDuration(2.0)
         CATransaction.setCompletionBlock { [weak self] in
-            guard let `self` = self  else { return }
-            var transform = self.colorLayer.affineTransform()
-            transform = transform.rotated(by: CGFloat.pi / 2)
-            self.colorLayer.setAffineTransform(transform)
+//            guard let `self` = self  else { return }
+//            var transform = self.colorLayer.affineTransform()
+//            transform = transform.rotated(by: CGFloat.pi / 2)
+//            self.colorLayer.setAffineTransform(transform)
         }
         let red = CGFloat.random(in: 0 ... 1)
         let green = CGFloat.random(in: 0 ... 1)
         let blue = CGFloat.random(in: 0 ... 1)
-        colorLayer.backgroundColor = UIColor(displayP3Red: red, green: green, blue: blue, alpha: 1).cgColor
+       // colorLayer.backgroundColor = UIColor(displayP3Red: red, green: green, blue: blue, alpha: 1).cgColor
+        redView.layer.backgroundColor = UIColor(displayP3Red: red, green: green, blue: blue, alpha: 1).cgColor
         CATransaction.commit()
     }
-    
+
+    @IBAction func skipToNextPage(_ sender: Any) {
+        performSegue(withIdentifier: "goto_detail", sender: self)
+    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let destination = segue.destination as? DetailViewController {
+            destination.callback = { tag in
+                print("======\(tag)======")
+            }
+        }
+    }
+
+    @IBAction func unwindSegue(segue: UIStoryboardSegue) {
+        if let vc = segue.source as? DetailViewController {
+            vc.callback?(vc.tag)
+        }
+    }
 }
 
